4c577b702af2d9dd52dc1a4eb99f814d1a9bbe97
ClexyT <eduardoaguilar10231@outlook.com>
2025-02-10 06:38:15 -0600

    minor changes

---
+++ app/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+    <div>Hi world</div>
+    </>
+  )
+}
+
+export default Main
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import  Main  from "../Main.jsx" ; 
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Movie Search App" },
+    { name: "description", content: "Welcome to your Movie Search!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Main />;
 }
